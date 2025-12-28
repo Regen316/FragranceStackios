227bde4ef25e30f3dd6c25cee86b26e30aa04c67
Stuart Nealy <regen.stu@gmail.com>
2025-12-28 01:07:01 -0800

    Initial Commit

---
+++ FragranceStackios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FragranceStackios
+//
+//  Created by Stuart Nealy Jr. on 12/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FragranceStackios/FragranceStackiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  FragranceStackiosApp.swift
+//  FragranceStackios
+//
+//  Created by Stuart Nealy Jr. on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FragranceStackiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
